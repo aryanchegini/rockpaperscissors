31559acd4bf7916b83caa2e42042ac56fc043fcd
Aryan Chegini <cheginiaryan.ac@gmail.com>
2021-11-13 19:30:41 +0000

    Final Working Game

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 let game = () => {
     let pScore = 0;
     let cScore = 0;
-    const choices = ['rock', 'paper', 'scissors'] 
+    const options = ['rock', 'paper', 'scissors']
+    let message = document.getElementById('message'); 
+
+    const updateScores = () => {
+        let playerScore = document.getElementById('player-score');
+        let compScore = document.getElementById('computer-score');
+        playerScore.innerText = pScore;
+        compScore.innerText = cScore;
+    }
+
+    const resetGame = () => {
+        //reset game back to original state
+        pScore = 0;
+        cScore = 0;
+        updateScores();   
+        message.innerText = 'try to beat the computer one more time';
+        let options = document.querySelector('.options');
+        options.classList.remove('inactive');
+        let resetButton = document.querySelector('.restart');
+        resetButton.classList.add('inactive');
+    }
     
     const compare = (playerChoice, computerChoice) => {
         if (playerChoice === computerChoice) {
@@ -33,21 +53,13 @@ let game = () => {
         }
     }
 
-    const updateScores = () => {
-        let playerScore = document.getElementById('player-score');
-        let compScore = document.getElementById('computer-score');
-        playerScore.innerText = pScore;
-        compScore.innerText = cScore;
-    }
-
     const updateScreen = (result) => {
-        let message = document.getElementById('message');
         if (result === 'tie') {
             message.innerText = 'looks like its a tie, go again';
             updateScores();
             return;
         } else if (result === true) {
-            message.innerText = 'sorry';
+            message.innerText = 'one step closer to victory';
             updateScores();
             return;
         } else if (result === false) {
@@ -57,16 +69,29 @@ let game = () => {
         }
     }
 
+    const checkForWinner = () => {
+        let buttons = document.querySelector('.options');
+        let restartButton = document.querySelector('.restart');
+        if (pScore === 3 || cScore === 3) {
+            buttons.classList.add('inactive');
+            restartButton.classList.remove('inactive');
+            //using ternary operator to determine what text to display
+            let finText = (pScore === 3) ? 'you are victorious!' : 'better luck next time buddy';
+            message.innerText = finText;
+        }
+        let rstBtn = document.getElementById('restart-game');
+        rstBtn.onclick = resetGame;
+    }
+
     const playgame = () => {
         let buttons = document.querySelectorAll('button');
         buttons.forEach(button => {
             button.addEventListener('click', () => {
-                //computer option
-                let cOption = choices[Math.floor(Math.random() * 3)];
-                //player option
+                let cOption = options[Math.floor(Math.random() * 3)];
                 let pOption = button.innerText;
                 let result = compare(pOption, cOption);
                 updateScreen(result);
+                checkForWinner();
             })
         })
     }
